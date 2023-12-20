8a2ad90efeb4b93bf550d6ebd51af2a37d525dfd
zmy <santuchuan23@gmail.com>
2023-12-20 11:52:42 +0800

    测试

--- src/main/java/com/sanzu/sanzuBook/appStart.java
+++ src/main/java/com/sanzu/sanzuBook/appStart.java
@@ -1,29 +1,29 @@
-//package com.sanzu.sanzuBook;
-//
-//import com.sanzu.sanzuBook.controller.Login.consoleFilter;
-//import com.sanzu.sanzuBook.controller.bookReview.WebSocketServerTest;
-//import jakarta.servlet.ServletContextEvent;
-//import jakarta.servlet.ServletContextListener;
-//import jakarta.servlet.annotation.WebListener;
-//
-//import java.net.InetSocketAddress;
-//@WebListener
-//public class appStart implements ServletContextListener {
-//    WebSocketServerTest webSocketServerTest;
-//
-//    @Override
-//    public void contextInitialized(ServletContextEvent sce) {
-//        webSocketServerTest=new WebSocketServerTest(new InetSocketAddress("172.19.30.111",1314));
-//        webSocketServerTest.start();
-//    }
-//
-//    @Override
-//    public void contextDestroyed(ServletContextEvent sce) {
-//        try {
-//            webSocketServerTest.stop();
-//            consoleFilter.webSocketServerTest.stop();
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//}
+package com.sanzu.sanzuBook;
+
+import com.sanzu.sanzuBook.controller.Login.consoleFilter;
+import com.sanzu.sanzuBook.controller.bookReview.WebSocketServerTest;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+
+import java.net.InetSocketAddress;
+@WebListener
+public class appStart implements ServletContextListener {
+    WebSocketServerTest webSocketServerTest;
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        webSocketServerTest=new WebSocketServerTest(new InetSocketAddress("172.19.30.111",1314));
+        webSocketServerTest.start();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        try {
+            webSocketServerTest.stop();
+            consoleFilter.webSocketServerTest.stop();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
